3a294d5eecfa23522ebd1fe1e2ad312feff6af2f
Martin Ziel <martin.ziel@googlemail.com>
2013-01-19 15:21:46 +0100

    SimpleDiceThrowDecider.GetCategory implemented to pass more tests, now returns the dice with the highest pip count and the highest occurrence

--- kniffel/src/com/schneide/kniffel/SimpleDiceThrowDecider.java
+++ kniffel/src/com/schneide/kniffel/SimpleDiceThrowDecider.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class SimpleDiceThrowDecider {
 
 	private static final int MAXIMUM_PIPS_ON_A_DICE = 6;
-	private static final int MINIMUM_PIPS_ON_A_DICE = 1;
+	private static final int TWO_PIPS = 2;
 	private List<Integer> thrownDice;
 
 	public SimpleDiceThrowDecider(List<Integer> thrownDice) {
@@ -13,16 +13,22 @@ public class SimpleDiceThrowDecider {
 	}
 
 	public int getCategory() {
-		for(int pip = MINIMUM_PIPS_ON_A_DICE; pip <= MAXIMUM_PIPS_ON_A_DICE; pip++){
-			if(allDiceHaveTheSame(pip)){
-				return pip;
-			}
+		int candidateForCategory = 1;
+		for(int pips = TWO_PIPS; pips <= MAXIMUM_PIPS_ON_A_DICE; pips++){
+			candidateForCategory = getMostSuitable(candidateForCategory, pips);
 		}
-		return 0;
+		return candidateForCategory;
 	}
 
-	private boolean allDiceHaveTheSame(int pip) {
-		return new DiceCounter(thrownDice).getCountOf(pip) == DiceThrower.ALL_DICE;
+	private int getMostSuitable(int candidateForCategory, int pips) {
+		if(occurrenceOf(pips) >= occurrenceOf(candidateForCategory)) {
+			return pips;
+		}
+		return candidateForCategory;
+	}
+
+	private int occurrenceOf(int pips) {
+		return new DiceCounter(thrownDice).getCountOf(pips);
 	}
 
 	public List<Integer> fixedDice() {
