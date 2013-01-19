e0ef57f341e10bb74d2b19477824f1f6217008c7
Martin Ziel <martin.ziel@googlemail.com>
2013-01-19 13:08:38 +0100

    implemented the DiceThrower, assuming that the dice are thrown randomly

--- kniffel/src/com/schneide/kniffel/DiceThrower.java
+++ kniffel/src/com/schneide/kniffel/DiceThrower.java
@@ -1,17 +1,29 @@
 package com.schneide.kniffel;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class DiceThrower {
 
+	private static final int ALL_DICE = 5;
+
 	public Collection<Integer> throwAllDice() {
-		// TODO Auto-generated method stub
-		return null;
+		return throwDice(ALL_DICE);
+		
 	}
 
-	public Collection<Integer> throwDice(int count) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Integer> throwDice(int numberOfDiceToThrow) {
+		Collection<Integer> thrownDice = new ArrayList<Integer>();
+		for(int diceToThrow = 0; diceToThrow < numberOfDiceToThrow; diceToThrow++) {
+			throwADiceAndAddItTo(thrownDice);
+		}
+		return thrownDice;
+	}
+
+	private void throwADiceAndAddItTo(Collection<Integer> thrownDice) {
+		int randomDiceThrow = (int)Math.floor((Math.random() * 6d) + 1d);
+		System.out.println(randomDiceThrow);
+		thrownDice.add(randomDiceThrow);
 	}
 
 }
