dbb805a50ea4502c97234e56290d2b4db9d645c5
Martin Ziel <martin.ziel@googlemail.com>
2013-01-19 16:30:52 +0100

    Clarified meaning of TurnResult parameters

--- kniffel/src/com/schneide/kniffel/TurnResult.java
+++ kniffel/src/com/schneide/kniffel/TurnResult.java
@@ -2,20 +2,20 @@ package com.schneide.kniffel;
 
 public class TurnResult {
 
-	private int i;
-	private int j;
+	private int category;
+	private int points;
 
-	public TurnResult(int i, int j) {
-		this.i = i;
-		this.j = j;
+	public TurnResult(int category, int points) {
+		this.category = category;
+		this.points = points;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + i;
-		result = prime * result + j;
+		result = prime * result + category;
+		result = prime * result + points;
 		return result;
 	}
 
@@ -28,9 +28,9 @@ public class TurnResult {
 		if (getClass() != obj.getClass())
 			return false;
 		TurnResult other = (TurnResult) obj;
-		if (i != other.i)
+		if (category != other.category)
 			return false;
-		if (j != other.j)
+		if (points != other.points)
 			return false;
 		return true;
 	}
